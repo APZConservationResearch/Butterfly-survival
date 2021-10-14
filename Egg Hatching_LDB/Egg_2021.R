b704459dc5e9e3b4a3e08487973293b41a4e6f52
Adam Grottoli <agrottoli@assiniboinepark.ca>
2021-10-14 08:19:31 -0500

    Updated visualisations and analysis

--- Egg Hatching_LDB/Egg_2021.R
+++ Egg Hatching_LDB/Egg_2021.R
@@ -4,41 +4,58 @@
 # Laura Burns
 # Email: lburns@assiniboinepark.ca
 
-# Description: This code summarises and visualises egg batch data by site and by individual.
+# Description: This code summarises and visualises egg batch data by site and by individual for egg batches up to 2021.
 
 # Libraries ---- 
 library(tidyverse)
 library(lubridate)
+library(lme4)
+library(ggeffects)
+library(MuMIn)
 
 # Setup ----
-data<- read.csv("POSK_eggbatches_2021.csv")
-sites <- read.csv("sites.csv")
+setwd("P:/Conservation_Research/Restricted/CRD/Research Projects/Pollinators/Poweshiek_Dakota_Garita_Skippers/R projects/Butterfly-survival")
 
-data<- data %>%
-  mutate("perc_hatch" = num_larva/number_eggs, "year" = dmy(date_collected)) %>%
+egg_batches <- read.csv("Raw_data/POSK_eggbatches_2021.csv")
+
+egg_batches <- egg_batches %>%
+  mutate("perc_hatch" = num_larva/number_eggs, "year" = dmy(date_collected), date_collected = dmy(date_collected), mate_day = dmy(mate_day)) %>%
   mutate_at(vars(year), funs(year))
-
-method <- data %>%
-  select(maternal_ID, method) %>% 
-  unique()
-
 
 # Summarizing Females -----------------------------------------------------
 
-female_hatch_summary<- data %>%
+female_hatch_summary <- egg_batches %>%
+  drop_na(maternal_lineage) %>%
+  filter(perc_hatch != 0.00) %>%
   group_by(maternal_ID, year) %>%
-  summarise (total_eggs = sum(number_eggs), percent_hatched = round((mean(perc_hatch))*100,2)) %>%
-  left_join(sites) %>% 
-  left_join(method)
+  mutate(total_eggs = sum(number_eggs), 
+         percent_hatched = round((mean(perc_hatch)) * 100,2), 
+        'site_method' = paste(maternal_lineage, method)) %>%
+  select(maternal_ID, maternal_lineage, method, total_eggs, percent_hatched, year) %>%
+  distinct()
 
-ggplot(female_hatch_summary, aes(maternal_ID, percent_hatched, fill = site)) +
-  geom_col()
+method_summary <- female_hatch_summary %>% 
+  drop_na(maternal_lineage) %>%
+  filter(percent_hatched != 0.00) %>%
+  group_by(method) %>%
+  summarise(percent_hatched = mean(percent_hatched))
 
+ggplot(female_hatch_summary, aes(method, percent_hatched)) +
+  geom_boxplot()
+
+site_summary <- female_hatch_summary %>% 
+  drop_na(maternal_lineage) %>%
+  filter(percent_hatched != 0.00) %>%
+  group_by(maternal_lineage) %>%
+  summarise(percent_hatched = mean(percent_hatched))
+
+ggplot(female_hatch_summary, aes(maternal_lineage, percent_hatched)) +
+  geom_boxplot()
 
 female_summary <- female_hatch_summary %>%
-  filter(percent_hatched != 0 & total_eggs >=10)
+  filter(percent_hatched != 0)
 
-ggplot(female_summary, aes(x = reorder(maternal_ID, percent_hatched), y = percent_hatched, fill = method)) +
+ggplot(female_summary, aes(x = reorder(maternal_ID, percent_hatched), y = percent_hatched)) +
   geom_col()+
   xlab("Maternal ID") +
   ylab("Percent hatched") +
@@ -49,7 +66,7 @@ ggplot(female_summary, aes(x = reorder(maternal_ID, percent_hatched), y = percen
   theme(legend.position = c(0.2, 0.8)) +
   theme(legend.text = element_text(size = 12))
 
-ggplot(female_summary, aes(x = percent_hatched, y=method, color = method)) +
+ggplot(female_summary, aes(x = percent_hatched, y = method, color = method)) +
   geom_point(size = 5) +
   xlab("Percent hatched") +
   ylab("Cross type") +
@@ -63,43 +80,78 @@ ggplot(female_summary, aes(x = percent_hatched, y=method, color = method)) +
 
 # Summarizing Sites -------------------------------------------------------
 
-site_summary <- data %>%
-  group_by(maternal_ID, year) %>%
-  left_join(sites, by = c("maternal_ID")) %>%
-  group_by(site, year) %>%
-  summarise(total_eggs = sum(number_eggs), total_hatched = sum(num_larva)) %>%
-  mutate("hatch_rate" = total_hatched/total_eggs, "siteID" = paste(year, site))
+site_summary <- female_hatch_summary %>%
+  group_by(maternal_lineage, year) %>%
+  summarise(percent_hatched = mean(percent_hatched)) %>%
+  mutate("siteID" = paste(year, maternal_lineage))
 
-ggplot(site_summary, aes(siteID, hatch_rate, fill = site))+
+ggplot(site_summary, aes(siteID, percent_hatched, fill = maternal_lineage)) +
   geom_col()
 
 
 # Time series -------------------------------------------------------------
 
-egg_output <- data %>%
-  mutate("date_collected" = dmy(date_collected)) %>%
-  filter(year == 2020) %>%
-  group_by(date_collected, maternal_ID) %>%
+egg_output <- egg_batches %>%
+  drop_na(mate_day) %>%
+  filter(year == 2021) %>%
+  mutate(BETWEEN0 = as.numeric(difftime(date_collected, (mate_day), 1)), days_after_mate = ifelse(is.na(BETWEEN0), 0, BETWEEN0)) %>%
+  group_by(days_after_mate, maternal_ID) %>%
   summarize(eggs_per_day = sum(number_eggs))
-
-ggplot(egg_output, aes(date_collected, eggs_per_day, color = maternal_ID))+
-  geom_line(size=1.5)
-
-
-
-# 2020 egg output ---------------------------------------------------------
-
-data2020 <- read_csv("2020POSKeggs.csv") %>% 
-  filter(!is.na(ID))
-
-
-ggplot(data2020, aes(Day, Eggs, color = ID))+
-  geom_point(size = 2)+
-  geom_smooth(method="glm", method.args = list(family = "poisson"))+
-  theme_bw(base_size = 14)+
-  theme(panel.grid.minor.x = element_blank())+
-  theme(legend.position = c(0.9, 0.8))+
-  theme(legend.background = element_rect(size=0.5, linetype="solid", colour="black"))+
-  xlab("Days after mating")+
-  ylab("Eggs collected")+
+  
+ggplot(egg_output, aes(days_after_mate, eggs_per_day, color = maternal_ID)) +
+  geom_point(size = 2) +
+  geom_smooth(method ="glm", method.args = list(family = "poisson"), alpha = 0.3) +
+  theme_bw(base_size = 14) +
+  theme(panel.grid.minor.x = element_blank()) +
+  theme(legend.position = c(0.9, 0.8)) +
+  theme(legend.background = element_rect(size = 0.5, linetype = "solid", colour = "black")) +
+  xlab("Days after mating") +
+  ylab("Eggs collected") +
   scale_x_continuous(n.breaks = 17)
+
+# Hatch rate and survival as a function of date layed ----
+
+lay_date_hatch <- egg_batches %>%
+  drop_na(mate_day) %>%
+  filter(year == 2021 | 2020) %>%
+  mutate(BETWEEN0 = as.numeric(difftime(date_collected, (mate_day),1)), days_after_mate = ifelse(is.na(BETWEEN0),0,BETWEEN0)) %>%
+  group_by(days_after_mate, maternal_ID) %>%
+  mutate(eggs_per_day = sum(number_eggs), hatched_per_day = sum(num_larva), day_hatch_rate = hatched_per_day/eggs_per_day) %>%
+  distinct(maternal_ID, day_hatch_rate, days_after_mate, eggs_per_day, year)
+
+
+ggplot(lay_date_hatch, aes(x = days_after_mate, y = day_hatch_rate, colour = maternal_ID)) +
+  geom_point(size = 2) +
+  geom_smooth(method ="glm", alpha = 0.1) +
+  theme_bw(base_size = 14) +
+  theme(panel.grid.minor.x = element_blank()) +
+  theme(legend.position = c(0.9, 0.8)) +
+  theme(legend.background = element_rect(size = 0.5, linetype = "solid", colour = "black")) +
+  xlab("Days after mating") +
+  ylab("Hatch rate") +
+  scale_x_continuous(n.breaks = 17)
+
+#  Test the random effects structure using a beyond optimal mode (i.e purposeful;ly overfit the data) 
+lay_date_hatch$maternal_ID <- factor(lay_date_hatch$maternal_ID) 
+lmer1 <- lmer(day_hatch_rate ~ days_after_mate + eggs_per_day + (as.factor(year)) + (1 + days_after_mate|maternal_ID), data = lay_date_hatch)
+lmer2 <- lmer(day_hatch_rate ~ days_after_mate + eggs_per_day + (as.factor(year)) + (1|maternal_ID), data = lay_date_hatch)
+lmer3 <- lm(day_hatch_rate ~ days_after_mate + eggs_per_day + (as.factor(year)), data = lay_date_hatch)
+
+AICc_out <- model.sel(lmer1, lmer2, lmer3)  # check which model has the lowest AIC
+
+pred.mm2 <- ggpredict(lmer3, terms = c("days_after_mate"))  # get the predicted values from the model
+pred.mm2 <- pred.mm2 %>% rename(c('x' = 'days_after_mate', 'predicted' = 'day_hatch_rate', 'group' = 'maternal_ID'))  # rename columns
+# lay_date_hatch$pred.lme <- predict(lmer3, newdata = lay_date_hatch, level = 0)  use pred.lme to plot random effects if using mixed effects model 
+
+plot1 <- ggplot(lay_date_hatch, aes(x = days_after_mate, y = day_hatch_rate, group = maternal_ID)) +
+  geom_rug(sides = 'b', alpha = 0) +
+  geom_smooth(data = lay_date_hatch, aes(y = day_hatch_rate, colour = (as.factor(year))), size = .8, alpha = 0.5, method = 'glm', se = FALSE) +
+  geom_line(data = pred.mm2, aes(x = days_after_mate, y = day_hatch_rate), size = 1.25) +
+  geom_ribbon(data = pred.mm2, aes(ymin = conf.low, ymax = conf.high,), alpha = 0.3) +
+  scale_x_continuous(breaks = c(0:20)) +
+  labs(x = "Days Between Mating and Egg Laying", y = "Hatch Rate", colour = "Year") + 
+  theme_classic()
+
+png("Hatch Rate.png", units = 'in', width = 7, height = 4, res = 600)
+plot1
+dev.off()
