4799b963e4d2d20de60693245504641860d1b7fc
Adam Grottoli <agrottoli@assiniboinepark.ca>
2022-08-02 13:56:56 -0500

    added adulthood summary

--- Overwinter_analysis/all_analysis.R
+++ Overwinter_analysis/all_analysis.R
@@ -21,6 +21,7 @@ library(MuMIn)
 setwd(paste0("P:/Conservation_Research/Restricted/CRD/Research Projects/Pollinators/",
              "Poweshiek_Dakota_Garita_Skippers/R projects/Butterfly-survival"))
 
+survival_colors <- c("#E69F00", "#993300")
 # Load data
 all_survival <- read.csv("./Overwinter_analysis/overwinter_full_data.csv")
 
@@ -50,26 +51,47 @@ ggplot() +
 ggplot() +
   geom_point(diapause_survival_GASK, mapping = aes(y = pre_mass, x = pre_length))
 
+ggplot(data = diapause_survival_GASK, aes(pre_length, survived_diapause)) +
+  geom_smooth(color = "steelblue", size = 1, method = "glm", method.args = list(family = "binomial") ) +
+  geom_point(color="steelblue")
+
 #POSK analysis
 
 adult_survival_POSK <- all_survival %>%
-  filter(species == "POSK", !is.na(diapause_days), !is.na(maternal_ID), !is.na(pre_mass), !is.na(pre_length), !is.na(post_mass), !is.na(DiapauseChamber), survived_diapause == 1) %>%
+  filter(species == "POSK", !is.na(diapause_days), !is.na(maternal_ID), 
+         !is.na(pre_mass), !is.na(pre_length), !is.na(post_mass), 
+         !is.na(DiapauseChamber), survived_diapause == 1) %>%
   mutate(maternal_ID = as.factor(maternal_ID),
          cohort = as.factor(cohort), 
-         pre_mass_g = pre_mass * 1000, post_mass_g = post_mass * 1000, mass_dif = post_mass_g - pre_mass_g, mass_prop = ifelse(mass_dif == 0, 1, mass_dif / pre_mass_g), DiapauseChamber = as.factor(paste(DiapauseChamber, cohort, sep = "_"))) %>%
-  mutate(plant = ifelse(is.na(plant), 0, (plant/100)))
+         pre_mass_g = pre_mass * 1000, post_mass_g = post_mass * 1000, 
+         mass_dif = post_mass_g - pre_mass_g, 
+         mass_prop = ifelse(mass_dif == 0, 1, mass_dif / pre_mass_g), 
+         DiapauseChamber = as.factor(paste(DiapauseChamber, cohort, sep = "_"))) 
 
 diapause_survival_POSK <- all_survival %>%
-  filter(species == "POSK", !is.na(diapause_days), !is.na(maternal_ID), !is.na(pre_mass), !is.na(pre_length), !is.na(DiapauseChamber), survival_to_diapause == 1) %>%
+  filter(species == "POSK", !is.na(diapause_days), !is.na(maternal_ID), 
+         !is.na(pre_mass), !is.na(pre_length), !is.na(DiapauseChamber), survival_to_diapause == 1) %>%
   mutate(maternal_ID = as.factor(maternal_ID),
          cohort = as.factor(cohort), 
-         pre_mass_g = pre_mass * 1000, DiapauseChamber = as.factor(paste(DiapauseChamber, cohort, sep = "_"))) %>%
-  mutate(plant = ifelse(is.na(plant), 0, (plant/100)))
+         pre_mass_g = pre_mass * 1000, DiapauseChamber = 
+           as.factor(paste(DiapauseChamber, cohort, sep = "_"))) 
 
+posk_adult_summary <- adult_survival_POSK %>%
+  group_by(maternal_ID) %>%
+  mutate(n_died = count(survival_diapause_adult == 1)) %>%
+  mutate(freq_died = n_died/n)         
 
-summary <- diapause_survival_POSK %>%
-  group_by(maternal_ID, survival_to_diapause)%>%
-summarise(count = n())         
+POSK_adult_summary <- adult_survival_POSK %>%
+  group_by(maternal_ID, survival_diapause_adult) %>%
+  summarize(N = n()) %>%
+  mutate(freq = N / sum(N))
+  ggplot(POSK_adult_summary, aes(x = maternal_ID,
+             y = freq,
+             fill = survival_diapause_adult)) 
+  geom_col() +
+  scale_y_continuous(labels = scales::percent) +
+  scale_fill_manual(values = survival_colors, labels = c("0", "1")) +
+  labs(x = "materal_ID", y = "Percent", fill = "surived to adult") +
   
 ggplot() +
   geom_jitter(diapause_survival_POSK, mapping = aes(y = pre_length, x = cohort), width = .1, height = .1)
@@ -85,6 +107,11 @@ ggplot() +
 
 ggplot() +
   geom_point(diapause_survival_POSK, mapping = aes(y = pre_mass, x = pre_length))
+
+ggplot(data = adult_survival_POSK, aes(pre_length, survived_diapause)) +
+  geom_smooth(color = "steelblue", size = 1, method = "glm", method.args = list(family = "binomial") ) +
+  geom_point(color="steelblue")
+
 
 VIF_test <- glmer(data = diapause_survival_POSK, survived_diapause ~ pre_mass + pre_length + (1|maternal_ID), family = binomial)
 
@@ -110,10 +137,7 @@ aic_GASK <- AICc(GASK_ran_test_1, GASK_ran_test_2, GASK_ran_test_3, GASK_ran_tes
 
 summary(GASK_ran_test_3)
 
-ggplot(data = diapause_survival_GASK, aes(pre_length, survived_diapause)) +
-  geom_smooth(color = "steelblue", size = 1, method = "glm", method.args = list(family = "binomial") ) +
-  geom_point(color="steelblue") + 
-  facet_wrap(~maternal_ID)
+
 
 
 
--- Poweshiek_fitness/fitness_wrangle.R
+++ Poweshiek_fitness/fitness_wrangle.R
@@ -10,6 +10,7 @@
 
 # Libraries ----
 library(tidyverse)
+library(lubridate)
 
 # Setup ----
 # Change to appropriate workspace
@@ -17,34 +18,73 @@ setwd(paste0("P:/Conservation_Research/Restricted/CRD/Research Projects/Pollinat
              "Poweshiek_Dakota_Garita_Skippers/R projects/Butterfly-survival"))
 
 # Load data
-egg_batches2018 <- read.csv('./Raw_data/egg_batches2018.csv', header = T)
-weekly_check2018 <- read.csv('./Raw_data/weekly_check2018.csv')
-weekly_check2019 <- read.csv('./Raw_data/weekly_check2019.csv')
-weekly_check2020 <- read.csv('./Raw_data/weekly_check2020.csv')
-weekly_check2021 <- read.csv('./Raw_data/weekly_check2021.csv')
-egg_collection2018 <- read.csv('./Raw_data/egg_collection2018.csv')
-egg_collection2019 <- read.csv('./Raw_data/egg_collection2019.csv')
-egg_collection2020 <- read.csv('./Raw_data/egg_collection2020.csv')
-egg_collection2021 <- read.csv('./Raw_data/egg_collection2021.csv')
-pupa_check2019 <- read.csv('./Raw_data/pupa_check2019.csv')
-pupa_check2018 <- read.csv('./Raw_data/pupa_check2018.csv')
-pupa_check2020 <- read.csv('./Raw_data/pupa_check2020.csv')
-larva_measurement2020 <- read.csv('./Raw_data/larva_measurement2020.csv')
-larva_measurement2019 <- read.csv('./Raw_data/larva_measurement2019.csv')
-larva_measurement2018 <- read.csv('./Raw_data/larva_measurement2018.csv')
-larva_measurement2021 <- read.csv('./Raw_data/larva_measurement2021.csv')
+egg_batches2018 <- read.csv('./Raw_data/POSK_egg_batches2018.csv', header = T)
+
+weekly_check2018 <- read.csv('./Raw_data/POSK_weekly_check2018.csv')
+
+weekly_check2019 <- read.csv('./Raw_data/POSK_weekly_check2019.csv')
+
+weekly_check2020 <- read.csv('./Raw_data/POSK_weekly_check2020.csv')
+
+weekly_check2021 <- read.csv('./Raw_data/POSK_weekly_check2021.csv')
+
+egg_collection2018 <- read.csv('./Raw_data/POSK_egg_collection2018.csv')
+
+egg_collection2019 <- read.csv('./Raw_data/POSK_egg_collection2019.csv')
+
+egg_collection2020 <- read.csv('./Raw_data/POSK_egg_collection2020.csv')
+
+egg_collection2021 <- read.csv('./Raw_data/POSK_egg_collection2021.csv')
+
+pupa_check2019 <- read.csv('./Raw_data/POSK_pupa_check2019.csv')
+
+pupa_check2018 <- read.csv('./Raw_data/POSK_pupa_check2018.csv')
+
+pupa_check2020 <- read.csv('./Raw_data/POSK_pupa_check2020.csv')
+
+larva_measurement2020 <- read.csv('./Raw_data/POSK_larva_measurement2020.csv')
+
+larva_measurement2019 <- read.csv('./Raw_data/POSK_larva_measurement2019.csv')
+
+larva_measurement2018 <- read.csv('./Raw_data/POSK_larva_measurement2018.csv') %>%
+  mutate(DiapauseChamber = parse_number(CurrentLocation))
+
+larva_measurement2021 <- read.csv('./Raw_data/POSK_larva_measurement2021.csv')
+
+GASK_larva_measurement2018 <- read.csv('./Raw_data/GASK_larva_measurement2018.csv') %>%
+  mutate(DiapauseChamber = parse_number(CurrentLocation))
+
+GASK_weekly_check2019 <- read.csv('./Raw_data/GASK_weekly_check2019.csv')
 
 
 # Functions ----
 
 # A function to make survival binary at each of the stages and bind mass data
 
-survival <- function (weekly_data, measurement_data, years_var, first_year, 
+survival <- function (weekly_data, measurement_data, cohort, 
                       week_of_diapause, week_after_diapause, final_week) {
   
-  mass_wide <- measurement_data %>% drop_na(Mass) %>%
-    mutate(pre_post = ifelse(Year == first_year, 'pre', 'post')) %>%
-    pivot_wider(id_cols = ID, names_from = pre_post, values_from = Mass)
+  measures_pre <- measurement_data %>% 
+    mutate(date_in = make_date(Year, Month, Day)) %>%
+    filter(Year == cohort) %>% 
+    group_by(ID) %>% 
+    filter(date_in == max(date_in)) %>% 
+    ungroup() %>% 
+    select(ID, Length, CurrentLocation, date_in, Mass, DiapauseChamber) %>% 
+    drop_na(Mass) %>% 
+    mutate(pre_length = Length, pre_mass = Mass) %>% 
+    select(-Length, -Mass)
+  measures_post <- measurement_data %>% 
+    mutate(date_out = make_date(Year, Month, Day)) %>%
+    filter(Year != cohort) %>% 
+    group_by(ID) %>% 
+    filter(date_out == min(date_out)) %>% 
+    ungroup() %>% 
+    select(ID, Length, date_out, Mass) %>% 
+    drop_na(Mass) %>% 
+    mutate(post_length = Length, post_mass = Mass) %>% 
+    select(-Length, -Mass)
+    
   weekly_data %>% 
     mutate(method = ifelse(method == "tube", "tube", "plant"), 
            survival_to_diapause = ifelse 
@@ -56,9 +96,13 @@ survival <- function (weekly_data, measurement_data, years_var, first_year,
            survival_after_diapause = ifelse(week_of_diapause == 'A', 1, 0),  # did it exit diapause
            survival_to_adult = ifelse(final_week == 'A', 1, 0)) %>% # did it survive to adult
     select(ID, year, maternal_ID, Site, egg_batch_ID, method, survival_to_diapause:survival_to_adult) %>%
-    left_join(mass_wide, by = 'ID', suffix = c(".x", ".y"))
+  left_join(measures_pre, by = 'ID', suffix = c(".x", ".y")) %>%
+  left_join(measures_post, by = 'ID', suffix = c(".x", ".y")) %>%
+    mutate(diapause_days = (date_out - date_in)) %>%
+    mutate(species = "POSK")
 }
 
+  
 # Function to count the number of days each individual spent as a pupa
 # Input the different characters for different stages of pupation 
 
@@ -76,6 +120,8 @@ pupa.time <- function (df, pupa_character1, pupa_character2, pupa_character3, pu
 # Create an egg batch column from the larva id coloumn if not already there
 weekly_check2018$egg_batch_ID <- substr(weekly_check2018$"ID",0 ,5)
 
+GASK_weekly_check2019$egg_batch_ID <- substr(GASK_weekly_check2019$"ID",0, 6)
+
 weekly_check2019$egg_batch_ID <- substr(weekly_check2019$"ID",0, 6)
 
 # Join maternal ID and site from the egg batch column if not already there and
@@ -88,6 +134,11 @@ weekly_survival2018 <- inner_join(weekly_check2018, egg_batches2018,
 
 weekly_survival2019 <- inner_join(weekly_check2019, egg_collection2019, 
                                   by = 'egg_batch_ID', suffix = c(".x", ".y")) %>% 
+  select(ID, maternal_ID, Site, egg_batch_ID, X22.Jul:Adult) %>%
+  mutate(year = 2019, method = "")
+
+GASK_weekly_survival2019 <- inner_join(GASK_weekly_check2019, GASK_egg_collection2019, 
+                                      by = 'egg_batch_ID', suffix = c(".x", ".y")) %>% 
   select(ID, maternal_ID, Site, egg_batch_ID, X22.Jul:Adult) %>%
   mutate(year = 2019, method = "")
 
@@ -104,25 +155,25 @@ weekly_survival2021 <- inner_join(weekly_check2021, egg_collection2021,
 
 # Run the survial function for each year 
 survival2018 <- survival(weekly_data = weekly_survival2018, measurement_data = 
-                           larva_measurement2018, first_year = 2018, week_of_diapause = 
+                           larva_measurement2018, cohort = 2018, week_of_diapause = 
                            weekly_survival2018$X12.Nov, week_after_diapause = 
                            weekly_survival2018$X06.May, final_week = 
                            weekly_survival2018$X08.Jul)
 
-survival2019 <- survival(weekly_data = weekly_survival2019, measurement_data = 
-                           larva_measurement2019, first_year = 2019,
+survival2019 <- survival(weekly_data = weekly_survival2019, measurement_data =
+                         larva_measurement2019, cohort = 2019,
                          week_of_diapause = weekly_survival2019$X07.Oct, 
                          week_after_diapause = weekly_survival2019$X27.Apr, 
                          final_week = weekly_survival2019$Adult)
 
 survival2020 <- survival(weekly_data = weekly_survival2020, measurement_data = 
-                           larva_measurement2020, first_year = 2020, 
+                           larva_measurement2020, cohort = 2020, 
                          week_of_diapause = weekly_survival2020$X05.Oct, 
                          week_after_diapause = weekly_survival2020$X26.Apr, 
                          final_week = weekly_survival2020$Adult)
 
 survival2021 <- survival(weekly_data = weekly_survival2021, measurement_data = 
-                           larva_measurement2021, first_year = 2021, 
+                           larva_measurement2021, cohort = 2021, 
                          week_of_diapause = weekly_survival2021$X25.Oct, 
                          week_after_diapause = weekly_survival2021$X25.Oct, 
                          final_week = weekly_survival2021$X25.Oct)
@@ -141,4 +192,3 @@ all_survival <- left_join((bind_rows(survival2018, survival2019, survival2020, s
 write.csv(all_survival, paste0("P:/Conservation_Research/Restricted/CRD/Research Projects/",
                                "Pollinators/Poweshiek_Dakota_Garita_Skippers/R projects/Butterfly-survival/Poweshiek_fitness/all_survival.csv"), 
           row.names = FALSE)
-
